fb996f4a14a3a3ed7ca5c0987df5575005c1b0d3
elias <eliasbayonajr@gmail.com>
2019-11-21 14:35:29 +0200

    Updates

---
+++ precache-manifest.ad74bcf66227c98b5f5fcd642c8300a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c60a755b12b12b1d89469df02046bf4",
+    "url": "/classEEE/index.html"
+  },
+  {
+    "revision": "2e0f16a690fad05261e1",
+    "url": "/classEEE/static/css/2.d9a8a5a7.chunk.css"
+  },
+  {
+    "revision": "04c8cfb80209893d9ee6",
+    "url": "/classEEE/static/css/main.391770e2.chunk.css"
+  },
+  {
+    "revision": "2e0f16a690fad05261e1",
+    "url": "/classEEE/static/js/2.102f34ae.chunk.js"
+  },
+  {
+    "revision": "04c8cfb80209893d9ee6",
+    "url": "/classEEE/static/js/main.9baec13b.chunk.js"
+  },
+  {
+    "revision": "3123391f9b784c150ed4",
+    "url": "/classEEE/static/js/runtime-main.06102409.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/classEEE/static/media/logo.25bf045c.svg"
+  }
+]);
