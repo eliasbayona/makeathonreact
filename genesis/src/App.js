3f5438623e56e8968bdaed8a9d1ec6795ca17fcc
elias <eliasbayonajr@gmail.com>
2019-11-21 14:17:47 +0200

    updates

--- genesis/src/App.js
+++ genesis/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
@@ -21,10 +21,31 @@ import AddIcon from '@material-ui/icons/Add';
 import LayersOutlinedIcon from '@material-ui/icons/LayersOutlined';
 import AirplanemodeActiveOutlinedIcon from '@material-ui/icons/AirplanemodeActiveOutlined';
 import BubbleChartOutlinedIcon from '@material-ui/icons/BubbleChartOutlined';
-
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Paper from '@material-ui/core/Paper';
+import Draggable from 'react-draggable';
+import {DropzoneArea} from 'material-ui-dropzone'
+import ReactMapGl from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 import logo from './logo.svg';
-import './App.css';
+import './css/css.css';
+
+function PaperComponent(props) {
+  return (
+    <Draggable cancel={'[class*="MuiDialogContent-root"]'}>
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -53,65 +74,138 @@ const useStyles = makeStyles(theme => ({
 function App() {
   const classes = useStyles();
   const [expanded, setExpanded] = React.useState(false);
+  const [localized, toggleLocalizedMode] = React.useState(false)
+  const [satelite, toggleSateliteMode] = React.useState(true)
+  const [analytics, toggleAnalyticsMode] = React.useState(false)
+  const [open, setOpen] = React.useState(false);
+  const [files, changeFiles] = React.useState([]);
+  const [viewport, setViewport] = React.useState({
+    latitude:-24.6600,
+    longitude:25.9031,
+    width:'100vw',
+    height:'100vh',
+    zoom:12
+  })
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   const handleExpandClick = () => {
     setExpanded(!expanded);
   };
+ const handleToggleLocalizedMode = () => {
+    toggleLocalizedMode(true);
+    toggleAnalyticsMode(false)
+    toggleSateliteMode(false)
+ };
 
+ const handleToggleAnalyticsMode = () => {
+  toggleLocalizedMode(false);
+  toggleAnalyticsMode(true)
+  toggleSateliteMode(false)
+ }
+
+ const handleToggleSateliteMode = () => {
+  toggleLocalizedMode(false);
+  toggleAnalyticsMode(false)
+  toggleSateliteMode(true)
+ }
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="xl">
-        <Card className={classes.card} style={{'marginTop':'2rem'}}> 
-          <CardHeader
-            
-            
-          />
-          <CardMedia
-            className={classes.media}
-            image="/static/images/cards/paella.jpg"
-            title="Paella dish"
-          />
-          <CardContent>
+    <ReactMapGl {...viewport} 
+     mapboxApiAccessToken="pk.eyJ1IjoidWJjaWN0IiwiYSI6ImNrMzhpMWk5MjA5NTQzbnJ0bnpybHo0ZHkifQ.QoKHB9zLuz-vFjMUHIkNig"
+     mapStyle="mapbox://styles/ubcict/ck38kuqw301l11cpkjwy3d1jo"
+     onViewportChange={ viewport => {
+       setViewport(viewport);
+     }}
+     >
+      <React.Fragment>
+          <CssBaseline />
           
-            <Typography variant="body2" color="textSecondary" component="p">
-              This impressive paella is a perfect party dish and a fun meal to cook together with your
-              guests. Add 1 cup of frozen peas along with the mussels, if you like.
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <IconButton
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
-          </CardActions>
+          <Container maxWidth="xl" className="map">
+            <Card className={classes.card} style={{'position':'absolute','marginTop':'2rem', 'background': '#171616de'}}> 
+            <CardHeader style={{'textAlign':'center'}}
           
-        </Card>
-        <div style={{'position':'absolute', 'top':'4rem', 'right':'2rem', 'display': 'inline-grid'}}>
-          <Fab style={{'margin':'.5rem'}} size="small" color="secondary" aria-label="add" className={classes.margin}>
-              <LayersOutlinedIcon />
-          </Fab>
-          <Fab style={{'margin':'.5rem'}} size="small" color="secondary" aria-label="add" className={classes.margin}>
-              <AirplanemodeActiveOutlinedIcon />
-          </Fab>
-          <Fab style={{'margin':'.5rem'}} size="small" color="secondary" aria-label="add" className={classes.margin}>
-              <BubbleChartOutlinedIcon />
-          </Fab>
-        </div>
-      </Container>
-    </React.Fragment>
+                subheader="Meta Data"
+            />
+            
+              <IconButton className={classes.iconButton} aria-label="menu">
+        
+              </IconButton>
+              <InputBase
+                className={classes.input}
+                placeholder="Search for location"
+                inputProps={{ 'aria-label': 'search for location' }}
+              />
+              <IconButton type="submit" className={classes.iconButton} aria-label="search">
+                <SearchIcon />
+              </IconButton>
+
+              <CardContent>
+                <Typography className="lcolor" variant="body2" color="white" component="p">
+                  Location:
+                </Typography>
+                <Typography className="data_color"  variant="body2" color="white" component="p">
+                  Gaborone
+                </Typography>
+                <Typography className="lcolor" variant="body2" color="white" component="p">
+                  Focus Area:
+                </Typography>
+                <Typography className="data_color"  variant="body2" color="white" component="p">
+                  0 0 0 0" N
+                </Typography>
+                <Typography className="data_color"  variant="body2" color="white" component="p">
+                  30 0 0 0" W
+                </Typography>
+                <Typography className="lcolor" variant="body2" color="white" component="p">
+                  Date & Time:
+                </Typography>
+                <Typography className="data_color" variant="body2" color="white" component="p">
+                  12 JUN 2019 
+                </Typography>
+              </CardContent>
+            </Card>
+            <div style={{'position':'absolute', 'top':'4rem', 'right':'2rem', 'display': 'inline-grid'}}>
+              <Fab onClick={() => toggleSateliteMode()}  style={{'margin':'.5rem'}} size="small" color="secondary" aria-label="add" className={classes.margin}>
+                  <LayersOutlinedIcon />
+              </Fab>
+              <Fab onClick={handleClickOpen} style={{'margin':'.5rem'}} size="small" color="secondary" aria-label="add" className={classes.margin}>
+                  <AirplanemodeActiveOutlinedIcon />
+              </Fab>
+              <Fab onClick={() => handleToggleAnalyticsMode()} style={{'margin':'.5rem'}} size="small" color="secondary" aria-label="add" className={classes.margin}>
+                  <BubbleChartOutlinedIcon />
+              </Fab>
+            </div>
+            <Dialog
+            maxWidth ="md"
+            fullWidth={true}
+            open={open}
+            onClose={handleClose}
+            PaperComponent={PaperComponent}
+            aria-labelledby="draggable-dialog-title"
+          >
+            <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+              Use localized data
+            </DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To subscribe to this website, please enter your email address here. We will send updates
+                occasionally.
+              </DialogContentText>
+              <DropzoneArea 
+                onChange={changeFiles.bind(this)}
+              />
+            </DialogContent>
+            
+          </Dialog>
+            
+          </Container>
+        </React.Fragment>
+    </ReactMapGl>
+    
   );
 }
 
